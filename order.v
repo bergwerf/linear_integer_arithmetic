e6a8a4b507a82d2018ded34ad8283319a449bc64
Herman Bergwerf <hermanbergwerf@gmail.com>
2021-02-11 14:06:10 +0100

    Add Order_lex2.

--- order.v
+++ order.v
@@ -6,12 +6,28 @@ From larith Require Import tactics notations utilities.
 Record Order {X} (cmp : X -> X -> comparison) := Order_spec {
   ord_eq : ∀x y, cmp x y = Eq <-> x = y;
   ord_opp : ∀x y, cmp x y = CompOpp (cmp y x);
-  ord_trans : ∀c x y z, cmp x y = c -> cmp y z = c -> cmp x z = c;
+  ord_trans_lt : ∀x y z, cmp x y = Lt -> cmp y z = Lt -> cmp x z = Lt;
 }.
 
 Arguments ord_eq {_ _}.
 Arguments ord_opp {_ _}.
-Arguments ord_trans {_ _}.
+Arguments ord_trans_lt {_ _}.
+
+Theorem ord_swap {X cmp} (ord : Order cmp) c (x y : X) :
+  cmp x y = c <-> cmp y x = CompOpp c.
+Proof.
+rewrite <-CompOpp_iff, <-(ord_opp ord); easy.
+Qed.
+
+Theorem ord_trans {X cmp} (ord : Order cmp) c (x y z : X) :
+  cmp x y = c -> cmp y z = c -> cmp x z = c.
+Proof.
+destruct c; intros.
+- apply (ord_eq ord) in H, H0; subst; apply (ord_eq ord); reflexivity.
+- apply (ord_trans_lt ord) with (y:=y); easy.
+- apply (ord_swap ord); apply (ord_swap ord) in H, H0; simpl in *.
+  apply (ord_trans_lt ord) with (y:=y); easy.
+Qed.
 
 Section Lexicographic_order_on_pairs.
 
@@ -21,29 +37,47 @@ Variable cmpB : B -> B -> comparison.
 Variable ordA : Order cmpA.
 Variable ordB : Order cmpB.
 
-Definition lex ab1 ab2 :=
-  match cmpA (fst ab1) (fst ab2) with
-  | Eq => cmpB (snd ab1) (snd ab2)
-  | Lt => Lt
-  | Gt => Gt
+Definition lex2 ab1 ab2 :=
+  match ab1, ab2 with
+  | (a1, b1), (a2, b2) =>
+    match cmpA a1 a2 with
+    | Eq => cmpB b1 b2
+    | Lt => Lt
+    | Gt => Gt
+    end
   end.
 
-Theorem lecicopraphic_order :
-  Order lex.
+Lemma lex2_Lt a1 b1 a2 b2 :
+  lex2 (a1, b1) (a2, b2) = Lt <->
+  cmpA a1 a2 = Lt \/ cmpA a1 a2 = Eq /\ cmpB b1 b2 = Lt.
 Proof.
-unfold lex; split.
-- intros [xa xb] [ya yb]; simpl; split.
-  + destruct (cmpA xa ya) eqn:Ha, (cmpB xb yb) eqn:Hb; try easy.
+unfold lex2; simpl.
+destruct (cmpA a1 a2), (cmpB b1 b2);
+split; intros []; auto; easy.
+Qed.
+
+Theorem Order_lex2 :
+  Order lex2.
+Proof.
+unfold lex2; split.
+- intros [a1 b1] [a2 b2]; simpl; split.
+  + destruct (cmpA a1 a2) eqn:Ha, (cmpB b1 b2) eqn:Hb; try easy.
     apply (ord_eq ordA) in Ha; apply (ord_eq ordB) in Hb; subst; reflexivity.
   + intros H; injection H; intros.
     apply (ord_eq ordA) in H1; apply (ord_eq ordB) in H0.
     rewrite H0, H1; reflexivity.
-- intros [xa xb] [ya yb]; simpl.
-  destruct (cmpA xa ya) eqn:Ha, (cmpB xb yb) eqn:Hb;
-  rewrite (ord_opp ordA) in Ha; rewrite (ord_opp ordB) in Hb;
-  apply CompOpp_iff in Ha, Hb; rewrite Ha, Hb; reflexivity.
-- intros c [xa xb] [ya yb] [za zb]; simpl.
-  (* Brute-force case analysis is a bit too heavy. *)
-Admitted.
+- intros [a1 b1] [a2 b2]; simpl.
+  destruct (cmpA a1 a2) eqn:Ha, (cmpB b1 b2) eqn:Hb;
+  rewrite (ord_swap ordA) in Ha; rewrite (ord_swap ordB) in Hb;
+  rewrite Ha, Hb; reflexivity.
+- intros [a1 b1] [a2 b2] [a3 b3]; simpl; intros.
+  apply lex2_Lt in H, H0; apply lex2_Lt; destruct H as [|[]], H0 as [|[]].
+  + left; apply (ord_trans ordA) with (y:=a2); easy.
+  + left; rewrite (ord_eq ordA) in H0; subst; easy.
+  + left; rewrite (ord_eq ordA) in H; subst; easy.
+  + right; split.
+    now apply (ord_trans ordA) with (y:=a2).
+    now apply (ord_trans ordB) with (y:=b2).
+Qed.
 
 End Lexicographic_order_on_pairs.
