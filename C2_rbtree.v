a2c3a444e1d4103574117e23edbd78aa17fc2b5d
Herman Bergwerf <hermanbergwerf@gmail.com>
2021-02-14 19:31:38 +0100

    Improve LLRB definition.

--- C2_rbtree.v
+++ C2_rbtree.v
@@ -1,4 +1,4 @@
-(* An implementation of the left-leaning red-black trees without deletion. *)
+(* A basic implementation of left-leaning red-black trees. *)
 (* See: https://mew.org/~kazu/proj/red-black-tree/ *)
 
 Require Import Lia.
@@ -10,41 +10,50 @@ Variable X : Type.
 Variable cmp : X -> X -> comparison.
 Hypothesis ord : Order cmp.
 
-Inductive rb_color := Rd | Bk.
+Inductive rb_color := Red | Black.
 Inductive rb_tree :=
   | Leaf
   | Fork (c : rb_color) (l : rb_tree) (x : X) (r : rb_tree).
 
+Notation Rd := (Fork Red).
+Notation Bk := (Fork Black).
+
 Definition rb_balance_l c l z r :=
-  match l with
+  match c, l with
   (* Rotation for double red fork. *)
-  | Fork Rd (Fork Rd xl x xr) y yr =>
-    Fork Bk (Fork Bk xl x xr) y (Fork Bk yr z r)
+  | Black, Rd (Rd xl x xr) y yr =>
+    Rd (Bk xl x xr) y (Bk yr z r)
   (* No rotation needed. *)
-  | _ => Fork c l z r
+  | _, _ => Fork c l z r
   end.
 
 Definition rb_balance_r c l y r :=
   match c, l, r with
   (* Propagate red fork upward. *)
-  | Bk, Fork Rd xl x xr, Fork Rd zl z zr =>
-    Fork Rd (Fork Bk xl x xr) y (Fork Bk zl z zr)
+  | Black, Rd xl x xr, Rd zl z zr =>
+    Rd (Bk xl x xr) y (Bk zl z zr)
   (* Rotation to make tree left-leaning. *)
-  | _, _, Fork Rd zl z zr =>
-    Fork c (Fork Rd l y zl) z zr
+  | _, _, Rd zl z zr =>
+    Fork c (Rd l y zl) z zr
   (* No rotation needed. *)
   | _, _, _ => Fork c l y r
   end.
 
-Fixpoint rb_insert x t :=
+Fixpoint rb_ins x t :=
   match t with
-  | Leaf => Fork Rd Leaf x Leaf
+  | Leaf => Rd Leaf x Leaf
   | Fork c l y r =>
     match cmp x y with
     | Eq => t
-    | Lt => rb_balance_l c (rb_insert x l) y r
-    | Gt => rb_balance_r c l y (rb_insert x r)
+    | Lt => rb_balance_l c (rb_ins x l) y r
+    | Gt => rb_balance_r c l y (rb_ins x r)
     end
+  end.
+
+Definition rb_insert x t :=
+  match rb_ins x t with
+  | Fork _ l x r => Bk l x r
+  | Leaf => Leaf (* never reached. *)
   end.
 
 Fixpoint rb_contains x t :=
@@ -64,48 +73,53 @@ Fixpoint rb_height t :=
   | Fork _ l _ r => max (rb_height l) (rb_height r)
   end.
 
+(* Prove that the algorithm obeys the invariants that keep the tree balanced. *)
 Section Tree_invariants.
 
-Fixpoint Rd_child Bk_root t :=
-  match t with
-  | Leaf => True
-  | Fork Bk l _ r => Rd_child False l /\ Rd_child False r
-  | Fork Rd l _ r => ¬Bk_root /\ Rd_child True l /\ Rd_child True r
+(* A red node has two black children; there are no red nodes on the right. *)
+Fixpoint LLRB_col c t :=
+  match c, t with
+  | _, Leaf => True
+  | Black, Bk l _ r =>  (LLRB_col Red l \/ LLRB_col Black l) /\ LLRB_col Black r
+  | Red, Rd l _ r => LLRB_col Black l /\ LLRB_col Black r
+  | _, _ => False
   end.
 
-Inductive Bk_height : nat -> rb_tree -> Prop :=
-  | Bk_height_Leaf :
-    Bk_height 0 Leaf
-  | Bk_height_Rd n l x r :
-    Bk_height n l -> Bk_height n r -> Bk_height n (Fork Rd l x r)
-  | Bk_height_Bk n l x r :
-    Bk_height n l -> Bk_height n r -> Bk_height (S n) (Fork Bk l x r).
-
-Fixpoint Left_leaning t :=  
+(* The root is red and its left child is as well. *)
+Notation Quasi_LLRB_col t :=
   match t with
-  | Leaf => True
-  | Fork Bk _ _ (Fork Rd _ _ _) => False
-  | Fork _ l _ r => Left_leaning l /\ Left_leaning r
+  | Rd l _ r => LLRB_col Red l /\ LLRB_col Black r
+  | _ => False
   end.
 
-Definition LLRB n t :=
-  Rd_child True t /\ Bk_height n t /\ Left_leaning t.
+Inductive Bk_balanced : nat -> rb_tree -> Prop :=
+  | Bk_balanced_Leaf :
+    Bk_balanced 0 Leaf
+  | Bk_balanced_Rd n l x r :
+    Bk_balanced n l -> Bk_balanced n r -> Bk_balanced n (Rd l x r)
+  | Bk_balanced_Bk n l x r :
+    Bk_balanced n l -> Bk_balanced n r -> Bk_balanced (S n) (Bk l x r).
+
+Definition LLRB p n t := LLRB_col p t /\ Bk_balanced n t.
+Definition Quasi_LLRB n t := Quasi_LLRB_col t /\ Bk_balanced n t.
 
 Theorem rb_max_height n t :
-  LLRB n t -> rb_height t <= 2 * n.
+  Bk_balanced n t -> rb_height t <= 2 * n.
 Proof.
-intros [_ [H _]]; revert H; revert n.
-induction t; simpl; intros; inv H; auto.
+revert n; induction t; simpl; intros; inv H; auto.
 all: apply IHt1 in H3; apply IHt2 in H6; lia.
 Qed.
 
-Theorem rb_insert_LLRB n x t :
-  LLRB n t -> LLRB n (rb_insert x t) \/ LLRB (S n) (rb_insert x t).
+Theorem LLRB_rb_insert n x t :
+  LLRB Black n t ->
+  LLRB Black n (rb_insert x t) \/
+  LLRB Black (S n) (rb_insert x t).
 Proof.
 Admitted.
 
 End Tree_invariants.
 
+(* Prove that the algorithm maintains a binary-search tree structure. *)
 Section Correct_insertion.
 
 Definition In_interval lwb upb y :=
