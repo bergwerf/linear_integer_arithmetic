d40a5874a7fe126cdd79a0aaa1064d85cba0ee2b
Herman Bergwerf <hermanbergwerf@gmail.com>
2021-02-14 14:57:23 +0100

    Add desired properties of rb_insert.

--- C2_rbtree.v
+++ C2_rbtree.v
@@ -57,4 +57,82 @@ Fixpoint rb_contains x t :=
     end
   end.
 
+Fixpoint rb_height t :=
+  match t with
+  | Leaf => 0
+  | Fork _ l _ r => max (rb_height l) (rb_height r)
+  end.
+
+Section Tree_invariants.
+
+Fixpoint Rd_child Bk_root t :=
+  match t with
+  | Leaf => True
+  | Fork Bk l _ r => Rd_child False l /\ Rd_child False r
+  | Fork Rd l _ r => ¬Bk_root /\ Rd_child True l /\ Rd_child True r
+  end.
+
+Inductive Bk_height : nat -> rb_tree -> Prop :=
+  | Bk_height_Leaf :
+    Bk_height 0 Leaf
+  | Bk_height_Rd n l x r :
+    Bk_height n l -> Bk_height n r -> Bk_height n (Fork Rd l x r)
+  | Bk_height_Bk n l x r :
+    Bk_height n l -> Bk_height n r -> Bk_height (S n) (Fork Bk l x r).
+
+Fixpoint Left_leaning t :=  
+  match t with
+  | Leaf => True
+  | Fork Bk _ _ (Fork Rd _ _ _) => False
+  | Fork _ l _ r => Left_leaning l /\ Left_leaning r
+  end.
+
+Definition LLRB n t :=
+  Rd_child True t /\ Bk_height n t /\ Left_leaning t.
+
+Theorem rb_max_height n t :
+  LLRB n t -> rb_height t < 2 * n.
+Proof.
+Admitted.
+
+Theorem rb_insert_LLRB n x t :
+  LLRB n t -> LLRB n (rb_insert x t) \/ LLRB (S n) (rb_insert x t).
+Proof.
+Admitted.
+
+End Tree_invariants.
+
+Section Correct_insertion.
+
+Definition In_interval lwb upb y :=
+  match lwb with
+  | Some x => cmp x y = Lt
+  | None => True
+  end /\
+  match upb with
+  | Some z => cmp y z = Lt
+  | None => True
+  end.
+
+Fixpoint BST lwb upb t :=
+  match t with
+  | Leaf => True
+  | Fork _ l y r =>
+    BST lwb (Some y) l /\
+    BST (Some y) upb r /\
+    In_interval lwb upb y
+  end.
+
+Theorem rb_insert_BST upb lwb x t :
+  BST lwb upb t -> BST lwb upb (rb_insert x t).
+Proof.
+Admitted.
+
+Theorem rb_insert_contains x t :
+  rb_contains x (rb_insert x t) = true.
+Proof.
+Admitted.
+
+End Correct_insertion.
+
 End Red_black_tree.
